432eec25a715ee6f9ff622295a275a6dd79e24bd
aprost <aprost@a085065d-6f10-0410-b0b9-eae7705693fd>
2006-12-24 13:49:37 +0000

    ""

---
+++ web/trunk/src/main/java/tutorials/directlink/pages/template.java
@@ -0,0 +1,18 @@
+/*
+ * Home.java
+ *
+ * Created on 23 décembre 2006, 19:14
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package tutorials.directlink.pages;
+
+import org.apache.tapestry.annotations.Persist;
+import org.apache.tapestry.html.BasePage;
+
+public abstract class template extends BasePage
+{
+    
+}
