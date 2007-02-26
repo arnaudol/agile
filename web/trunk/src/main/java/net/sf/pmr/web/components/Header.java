7786b74a13e105e8ab1e9c950ee126ea4c028ec9
aprost <aprost@a085065d-6f10-0410-b0b9-eae7705693fd>
2007-02-26 21:44:54 +0000

    ""

---
+++ web/trunk/src/main/java/net/sf/pmr/web/components/Header.java
@@ -0,0 +1,22 @@
+/*
+ * Home.java
+ *
+ * Created on 23 décembre 2006, 19:14
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package net.sf.pmr.web.components;
+
+import org.apache.tapestry.BaseComponent;
+import org.apache.tapestry.IAsset;
+import org.apache.tapestry.IPage;
+import org.apache.tapestry.annotations.Asset;
+import org.apache.tapestry.annotations.Parameter;
+import org.apache.tapestry.annotations.ComponentClass;
+
+@ComponentClass(allowBody=false)
+public abstract class Header extends BaseComponent {
+
+}
