55f7787f0e7127798e5c438ec9241f4362dd4892
aprost <aprost@a085065d-6f10-0410-b0b9-eae7705693fd>
2007-04-15 22:10:01 +0000

    ""

---
+++ web/trunk/src/main/java/net/sf/pmr/web/pages/select/ProjectSelectionModel.java
@@ -0,0 +1,61 @@
+/*
+ * ProjectSelectionModel.java
+ *
+ * Created on 30 mars 2007, 21:11
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package net.sf.pmr.web.pages.select;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import net.sf.pmr.core.CoreObjectFactory;
+import net.sf.pmr.core.domain.project.Project;
+import org.apache.tapestry.form.IPropertySelectionModel;
+
+/**
+ *
+ * @author arnaud
+ */
+public class ProjectSelectionModel implements IPropertySelectionModel  {
+    
+    private List<Project> projects;
+    
+    /** Creates a new instance of ProjectSelectionModel */
+    public ProjectSelectionModel(int userPersistanceId) {
+
+        // get the projets for the user
+        Set<Project> projectSet = CoreObjectFactory.getProjectService().findForAUser(userPersistanceId);
+        
+        // put the project in a list
+        projects = new ArrayList<Project>(projectSet);
+         
+    }
+
+    public Object translateValue(String value) {
+           return getOption(Integer.parseInt(value));
+    }
+
+    public String getValue(int index) {
+        return Integer.toString(index);
+    }
+
+    public Object getOption(int index) {
+        return projects.get(index);
+    }
+
+    public String getLabel(int index) {
+        return projects.get(index).getName();
+    }
+
+    public int getOptionCount() {
+        return projects.size(); 
+        }
+    
+    }
+    
+    
+    
