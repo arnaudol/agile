19902e7cbe422870351ff5e76379c5079344ae67
aprost <aprost@a085065d-6f10-0410-b0b9-eae7705693fd>
2007-05-13 19:51:28 +0000

    création de la première version de l'onglet dynamique

---
+++ web/trunk/src/main/java/net/sf/pmr/web/components/TabsContent.java
@@ -0,0 +1,43 @@
+/*
+ * TabsContent.java
+ * 
+ * Created on 13 mai 2007, 14:47:11
+ * 
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package net.sf.pmr.web.components;
+
+/**
+ *
+ * @author arnaud
+ */
+public class TabsContent {
+    
+    
+    private String label;
+    
+    private String url;    
+    
+
+    public TabsContent() {
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }    
+    
+}
